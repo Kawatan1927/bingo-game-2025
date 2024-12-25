61eb979a2f96f8ae5ffe0ada1a21b7471f0b9ea4
yahagi <hikaru.yahagi@ei-sol.co.jp>
2024-12-25 21:18:06 +0900

    ビルド設定記述の追加

--- main.js
+++ main.js
@@ -22,8 +22,11 @@ app.on('window-all-closed', function() {
 // Electronの初期化完了後に実行
 app.on('ready', function() {
   // メイン画面の表示。ウィンドウの幅、高さを指定できる
-  mainWindow = new BrowserWindow({width: 1920, height: 1080, center: true, resizable: false, frame: false});
+  mainWindow = new BrowserWindow({width: 1920, height: 1080, center: true, resizable: false, frame: true});
   mainWindow.loadURL('file://' + __dirname + '/bingo.html');
+
+  // デベロッパーツールの表示(必要だったらコメントアウトを外してください)
+  // mainWindow.webContents.openDevTools();
 
   // ウィンドウが閉じられたらアプリも終了
   mainWindow.on('closed', function() {
